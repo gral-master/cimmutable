bc401b2697ddd946317f89529ecd9e741a781461
Anastasios Doumoulakis <a.doumoulakis@gmail.com>
2016-12-08 22:14:55 +0100

    Added first version of doxygen config file Added basic RRB-vector sucture to header file Fixed indentation and spacing in headers

--- include/map.h
+++ include/map.h
@@ -11,24 +11,18 @@ imc_map_t* imc_map_create();
 
 int imc_map_size(imc_map_t* map);
 
-imc_map_t* imc_map_update(imc_map_t* map,
-			  imc_key_t* key,
-			  imc_data_t* data);
+imc_map_t* imc_map_update(imc_map_t* map, imc_key_t* key, imc_data_t* data);
 
-imc_data_t* imc_map_lookup(imc_map_t* map,
-			   imc_key_t* key);
+imc_data_t* imc_map_lookup(imc_map_t* map, imc_key_t* key);
 
-imc_map_t* imc_map_remove(imc_map_t* map,
-			  imc_key_t* key,
-			  imc_data_t** data);
+imc_map_t* imc_map_remove(imc_map_t* map, imc_key_t* key,imc_data_t** data);
 
 imc_key_t*[] imc_map_keys(imc_map_t* map);
 
 // iterator ?
 typedef imc_map_iterator_t;
 
-void imc_map_iterate(imc_map_t* map,
-		     imc_map_iterator_t *iter);
+void imc_map_iterate(imc_map_t* map, imc_map_iterator_t *iter);
 
 /* user-side memory management */
 
