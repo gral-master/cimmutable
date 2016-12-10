f895050c33b1091a57e0d2334cbb17ff1790740b
Guillaume Hivert <guillaume.hivert@outlook.com>
2016-12-11 00:04:41 +0100

    :white_check_mark:

--- Makefile
+++ Makefile
@@ -1,21 +1,33 @@
 CC = clang
 CFLAGS = -Wall -Wextra -std=gnu11
-LDFLAGS = -lcheck
+LDFLAGS = -Iinclude
 
-OBJ = avl.o finger.o rrb_vector.o
+EXEC_TESTS = rrb_tests avl_tests finger_tests
+SRC_TESTS = $(addsuffix .c, $(EXEC_TESTS))
+LDTESTS = -lcheck -Iinclude -limc -Llib
+OBJ = $(addprefix bin/, avl.o finger.o rrb_vector.o)
 
 all: lib
-	mkdir -p bin
-	mkdir -p lib
 
 lib: compile $(OBJ)
 	ar rcs lib/libimc.a $(OBJ)
 
 compile:
-	cd src: make -k
+	mkdir -p bin
+	mkdir -p lib
+	cd src; make -k
+
+tests: lib compile_tests
+
+compile_tests: $(addprefix tests/, $(SRC_TESTS))
+	for obj in $(basename $(SRC_TESTS)) ; do \
+		$(CC) $(CFLAGS) $(LDTESTS) -o $(addprefix bin/, $$obj) tests/$$obj.c; \
+	done
 
 clean:
 	rm -rf bin
 
 mrproper:
 	rm -rf bin lib
+
+.PHONY: all lib compile tests clean mrproper
--- tests/avl_tests.c
+++ tests/avl_tests.c
@@ -2,49 +2,49 @@
 #include <vector.h>
 #include <stdlib.h>
 
-START_TEST(rrb_create)
+START_TEST(avl_create)
 {
-    imc_vector_t* rrb = imc_rrb_new();
+    imc_vector_t* avl = imc_avl_new();
 
     // Check for the creation.
-    ck_assert_ptr_ne(rrb, NULL);
+    ck_assert_ptr_ne(avl, NULL);
 
-    rrb->unref(rrb);
+    avl->unref(avl);
 }
 END_TEST
 
-START_TEST(rrb_push)
+START_TEST(avl_push)
 {
     imc_data_t* data   = malloc(sizeof *data);
-    imc_vector_t* rrb1 = imc_rrb_new();
+    imc_vector_t* avl1 = imc_avl_new();
 
     // Check for the new AVL.
-    imc_vector_t* rrb2 = rrb1->push(rrb1, data);
-    ck_assert_ptr_ne(rrb2, NULL);
+    imc_vector_t* avl2 = avl1->push(avl1, data);
+    ck_assert_ptr_ne(avl2, NULL);
 
     // Check for the value.
-    imc_data_t* lookup = rrb2->lookup(rrb2, 1);
+    imc_data_t* lookup = avl2->lookup(avl2, 1);
     ck_assert_ptr_eq(data, lookup);
 
     // Cleana little bit.
-    rrb1->unref(rrb1);
-    rrb2->unref(rrb2);
+    avl1->unref(avl1);
+    avl2->unref(avl2);
 }
 END_TEST
 
-Suite* rrb_suite(void) {
+Suite* avl_suite(void) {
     Suite* suite   = suite_create("AVL");
     TCase* tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, rrb_create);
-    tcase_add_test(tc_core, rrb_push);
+    tcase_add_test(tc_core, avl_create);
+    tcase_add_test(tc_core, avl_push);
     suite_add_tcase(suite, tc_core);
 
     return suite;
 }
 
 int main(void) {
-    Suite*   suite = rrb_suite();
+    Suite*   suite = avl_suite();
     SRunner* suite_runner = srunner_create(suite);
 
     srunner_run_all(suite_runner, CK_NORMAL);
