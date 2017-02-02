cb4dbcda2de9d411791a9c2928db07551c49bec2
Guillaume Hivert <guillaume.hivert@outlook.com>
2017-02-02 20:45:42 +0100

    test bench

--- src/avl/genbench.pl
+++ src/avl/genbench.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-open *STDOUT, '>', 'tests/303_int_vec_push_10000.bench' or die $!;
+open *STDOUT, '>', 'tests/703_int_vec_split_10000.bench' or die $!;
 
 print <<HEADERS;
 # Test benchmark.
@@ -23,21 +23,22 @@ int
     
 HEADERS
     
-    for (0 .. 10000) {
-	$v = int rand 10000;
+for (0 .. 10000) {
+    $v = int rand 10000;
     if ($_ % 2 == 0) {
-	print "1 = push(0,$v)\n";
+	print "0 = push(0,$v)\n";
     } else {
-	print "0 = push(1,$v)\n";
+	print "0 = push(0,$v)\n";
     }
 }
 
-#for (0 .. 10000) {
- #   if ($_ % 2 == 0) {
-#	print "0 = pop(1)\n";
-#	print "unref(1)\n";
- #   } else {
-#	print "1 = pop(0)\n";
-#	print "unref(0)\n";	
- #   }
-#}
+print "0 = split(0, 5, 1, 2)\n";
+
+# for (0 .. 10) {
+#     $v = int rand 10000;
+#     if ($_ % 2 == 0) {
+# 	print "0 = update(0, 2, $v)\n";
+#     } else {
+# 	print "0 = update(0, 3, $v)\n";
+#     }
+# }
